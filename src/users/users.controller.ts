dd7c0a90100500e3b773efa471197a35003e77a1
Ruby Chang <dev.rubimoon@gmail.com>
2022-03-28 16:57:06 +0800

    feat(users): add CRUD

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,5 +1,15 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { CreateUserDto } from './dtos/createUserDto';
+import { UpdateUserDto } from './dtos/updateUserDto';
 import { UsersService } from './users.service';
 
 @Controller('auth')
@@ -11,8 +21,23 @@ export class UsersController {
     await this.usersService.create(body.email, body.password);
   }
 
-  //TODO findUser
-  //TODO findAllUsers
-  //TODO updateUser
-  //TODO removeUser
+  @Get('/:id')
+  async findUser(@Param('id') id: string) {
+    return await this.usersService.findOne(parseInt(id));
+  }
+
+  @Get()
+  async findAllUsers(@Query('email') email: string) {
+    return await this.usersService.find(email);
+  }
+
+  @Patch('/:id')
+  async updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+    await this.usersService.update(parseInt(id), body);
+  }
+
+  @Delete('/:id')
+  async removeUser(@Param('id') id: string) {
+    return await this.usersService.remove(parseInt(id));
+  }
 }
