dd8e23d378a23f2ce0c928e30c8c0668c55ecced
Ruby Chang <dev.rubimoon@gmail.com>
2022-03-30 13:15:38 +0800

    feat(auth): add session middleware

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as session from 'express-session';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(
+    session({
+      secret: 'asdfg',
+      resave: false,
+      saveUninitialized: false,
+      cookie: { secure: false },
+    }),
+  );
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(3000);
 }
