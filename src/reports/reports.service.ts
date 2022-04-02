3eeb79b7eb8783e85b73bb7db804855788149720
Ruby Chang <dev.rubimoon@gmail.com>
2022-04-02 17:22:43 +0800

    feature(reports): add reports estimation

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -2,14 +2,14 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Report, User } from '../entities';
-import { CreateReportDto } from './dtos';
+import { CreateReportDto, GetEstimateDto } from './dtos';
 
 @Injectable()
 export class ReportsService {
   constructor(@InjectRepository(Report) private repo: Repository<Report>) {}
 
-  create(user: User, report: CreateReportDto) {
-    const entity = this.repo.create(report);
+  create(user: User, dto: CreateReportDto) {
+    const entity = this.repo.create(dto);
     entity.user = user;
     return this.repo.save(entity);
   }
@@ -21,5 +21,21 @@ export class ReportsService {
 
     entity.approved = approved;
     return this.repo.save(entity);
+  }
+
+  createEstimate({ make, model, lng, lat, year, mileage }: GetEstimateDto) {
+    return this.repo
+      .createQueryBuilder()
+      .select('AVG(price)', 'price')
+      .where('make= :make', { make })
+      .andWhere('model= :model', { model })
+      .andWhere('lng - :lng BETWEEN -5 AND 5', { lng })
+      .andWhere('lat - :lat BETWEEN -5 and 5', { lat })
+      .andWhere('year - :year BETWEEN -3 and 3', { year })
+      .andWhere('approved IS TRUE')
+      .orderBy('ABS(mileage - :mileage)', 'DESC')
+      .setParameters({ mileage })
+      .limit(3)
+      .getRawOne();
   }
 }
